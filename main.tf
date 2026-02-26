748afd42826372afaaa87639c05e614c2fdc4093
Satvik Varun <satvikvarun@gmail.com>
2026-02-27 02:01:59 +0530

    Fix terraform formatting

--- main.tf
+++ main.tf
@@ -16,21 +16,21 @@ module "rds" {
 }
 
 module "ec2" {
-  source               = "./modules/ec2"
-  subnets              = module.vpc.private_app_subnets
-  frontend_sg          = module.security.frontend_sg
-  backend_sg           = module.security.backend_sg
-  rds_endpoint         = module.rds.endpoint
-  db_username          = var.db_username
-  db_password          = var.db_password
-  dockerhub_username   = var.dockerhub_username
+  source             = "./modules/ec2"
+  subnets            = module.vpc.private_app_subnets
+  frontend_sg        = module.security.frontend_sg
+  backend_sg         = module.security.backend_sg
+  rds_endpoint       = module.rds.endpoint
+  db_username        = var.db_username
+  db_password        = var.db_password
+  dockerhub_username = var.dockerhub_username
 }
 
 module "alb" {
-  source              = "./modules/alb"
-  public_subnets      = module.vpc.public_subnets
-  alb_sg              = module.security.alb_sg
-  vpc_id              = module.vpc.vpc_id
-  frontend_instance   = module.ec2.frontend_id
-  backend_instance    = module.ec2.backend_id
+  source            = "./modules/alb"
+  public_subnets    = module.vpc.public_subnets
+  alb_sg            = module.security.alb_sg
+  vpc_id            = module.vpc.vpc_id
+  frontend_instance = module.ec2.frontend_id
+  backend_instance  = module.ec2.backend_id
 }
